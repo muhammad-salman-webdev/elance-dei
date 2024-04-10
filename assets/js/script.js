8721cf76cea413567d4eb50b8e398543250d95f1
Muhammad Salman <muhammadsalman.webdev@gmail.com>
2024-04-10 11:34:06 +0500

    Language Change Feature Added Successfully
    Language Change Button is working only in Index.html for testing.

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,3 +31,63 @@ function setHeightOfSubMenu(menuItem) {
     .querySelector("ul")
     .style.setProperty("--height", subMenuHeight + "px");
 }
+
+// Toggling the Language
+
+const defaultImageFolder = "eng_images";
+const newImageFolder = "fr_images";
+
+const changeLangBtn = document.getElementById("change_lang_btn");
+let lang = changeLangBtn.getAttribute("prefferedLang");
+
+changeLangBtn.addEventListener("click", langBtnClicked);
+
+// Checking and Setting the Language Variable in Browser Local Storage
+function checkingAndSettingLangInLocalStorage() {
+  if (localStorage.getItem("lang") === null) {
+    localStorage.setItem("lang", lang);
+  } else {
+    lang = localStorage.getItem("lang");
+  }
+}
+checkingAndSettingLangInLocalStorage();
+
+if (lang == "fr") {
+  changeLangBtn.innerText = "EN";
+  changeContentLang();
+}
+
+// Default Runner
+function langBtnClicked() {
+  if (lang === "eng") {
+    changeLangBtn.innerText = "EN";
+    lang = "fr";
+  } else {
+    changeLangBtn.innerText = "FR";
+    lang = "eng";
+  }
+  menuBtn.classList.remove("active");
+  changeContentLang();
+  localStorage.setItem("lang", lang);
+}
+
+function changeContentLang() {
+  // Changing the text of Elements
+  const elems = document.querySelectorAll("[fr-lang]");
+  elems.forEach((elem) => {
+    const curText = elem.textContent;
+    const newText = elem.getAttribute("fr-lang");
+    elem.textContent = newText;
+    elem.setAttribute("fr-lang", curText);
+  });
+  // Changing the Image URL
+  const images = document.querySelectorAll("img[change-image-lang]");
+  images.forEach((image) => {
+    const curURL = image.src;
+    if (curURL.includes(defaultImageFolder)) {
+      image.src = curURL.split(defaultImageFolder).join(newImageFolder);
+    } else {
+      image.src = curURL.split(newImageFolder).join(defaultImageFolder);
+    }
+  });
+}
